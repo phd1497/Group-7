40330de58c033b685a8e57888e2c4a52eb86ce9b
phd1497 <72797290+phd1497@users.noreply.github.com>
2024-12-13 14:45:24 -0600

    Hoang_Dat change styled of text

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text>Hello! My Name is Raj Trusharkumar Patel</Text>
-      <Text>And My name is Hoang Dat Phan</Text>
+
+      <Text style={styles.names}>And My name is Hoang Dat Phan</Text>
 
       <StatusBar style="auto" />
     </View>
@@ -16,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dda15e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  names:{
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "thistle",
+    borderRadius: 50,
+    color:'blue',
+  }
 });
